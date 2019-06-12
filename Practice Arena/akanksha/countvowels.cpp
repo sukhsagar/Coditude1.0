adda03d56a88dfd36c00e3bb319650754e0b5ff7
AakankshaHanda <37653412+AakankshaHanda@users.noreply.github.com>
2019-06-12 23:18:04 +0530

    count no of vowels

---
+++ Practice Arena/akanksha/countvowels.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	char str[30]="how are you";
+	int i=0,count=0;
+	while(str[i]!='\0'){
+		switch(str[i]){
+			case 'a':
+				case 'e':
+					case 'i':
+						case 'o':
+							case 'u':
+								count++;
+							}
+							i++;
+							
+							
+		}
+		cout<<count++;
+	}
+
