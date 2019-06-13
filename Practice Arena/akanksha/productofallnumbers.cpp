c97eb2e6bb29992622af5046929dd8b722b56631
AakankshaHanda <37653412+AakankshaHanda@users.noreply.github.com>
2019-06-13 23:49:53 +0530

    decimaltobinary

---
+++ Practice Arena/akanksha/productofallnumbers.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	int num,ldigit,temp=1;
+	cout<<"enter any number";
+	cin>>num;
+	while(num/10>0){
+		while(num>0){
+			ldigit=num%10;
+			temp=ldigit;
+			temp=temp*ldigit;
+			num=num/10;
+			
+		}
+		num=temp;
+		temp=1;
+		
+		
+	}
+	cout<<"number is"<<temp;
+}
