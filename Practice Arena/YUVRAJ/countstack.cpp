9c93b4a21e2d9f38de679db8f7e455b5bc437b68
Yuvraj Singh <Yuvrajcing@gmail.com>
2019-06-21 15:04:01 +0530

    Update countstack.cpp

--- Practice Arena/YUVRAJ/countstack.cpp
+++ Practice Arena/YUVRAJ/countstack.cpp
@@ -1,17 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int a[]={100,80,60,70,60,75,85};
-	int size=7,count=0;
+	int a[]={10,90,60,70,60,75,85};
+	int size=7,count=1;
 	for(int j=size-1;j>=0;j--){
 	for(int i=j-1;i>=0;i--){
 		if(a[j]>a[i]){
 			count++;
 		}
-		
+		if(a[i]>a[j])
+		break;
 	}
-	cout<<"count of "<<a[j]<<"is "<<count+1<<endl;
-	count=0;
+	cout<<"count of "<<a[j]<<"is "<<count<<endl;
+	count=1;
 	}
 	return 0;
 }
