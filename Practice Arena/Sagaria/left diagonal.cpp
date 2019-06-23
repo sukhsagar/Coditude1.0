d41de463ff9dc89b52310b4019b0925e79c8db3b
sagariaverma <51718884+sagariaverma@users.noreply.github.com>
2019-06-24 00:44:00 +0530

    please check right daigonal

---
+++ Practice Arena/Sagaria/left diagonal.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+	int i,j,n,sum=0,m,a[50][50];
+	cout<<"enter the size  of rows and columns u want";
+	cin>>m>>n;
+	cout<<"enter the elements into an array";
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++)
+		{
+			cin>>a[i][j];
+			}
+	}
+	cout<<"the elements of matrix are- ";
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++)
+		{
+			cout<<a[i][j];
+		}
+			cout<<"\n";
+			
+	}
+
+     for(i=0;i<m;i++){
+     	
+     
+				sum=sum+a[i][i];
+			}
+		
+		
+		
+  cout<<" the sum of the left diagonal is "<<sum;
+
+}
